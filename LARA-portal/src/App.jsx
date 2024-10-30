483ff7231816d2cc3f42fee2aa1c6dd3683dc81c
NobreRocha <thagonobre567@gmail.com>
2024-10-30 16:00:33 -0300

    colocando-o-conteudo
    alto explicativo

--- LARA-portal/src/App.jsx
+++ LARA-portal/src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <>
       <Header/>
+      <div className='conteudo'>
+        <h2>O que é <br/> L.A.R.A ?</h2>
+        <p>L.A.R.A ou Laboratório de Automação e Robótica Ateneu. <br/> Somos uma incubadora de projetos para IoT, sistemas embarcados e robótica.</p>
+      </div>
     </>
   )
 }
