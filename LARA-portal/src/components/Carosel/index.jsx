f15fb949ff89044502749e753a7b9fa8c0449e06
NobreRocha <thagonobre567@gmail.com>
2024-11-06 15:15:38 -0300

    coloquei outro carrossel
    tentando colocar as imgs porem n está indo

--- LARA-portal/src/components/Carosel/index.jsx
+++ LARA-portal/src/components/Carosel/index.jsx
@@ -4,26 +4,26 @@ function Carosel() {
 
   return (
     <>
-        <div id="carouselExampleFade" class="carousel slide carousel-fade">
-        <div class="carousel-inner">
+        <div id="carouselExample" class="carousel slide">
+          <div class="carousel-inner">
             <div class="carousel-item active">
-            <img src="../imgs/img1.jpg" class="d-block w-100" alt="imagem 1"></img>
+              <img src="../imgs/img1.jpg" class="d-block w-100" alt="..."/>
             </div>
             <div class="carousel-item">
-            {/* <img src="..." class="d-block w-100" alt="..."> colocar a imagem*/}
+              <img src="..." class="d-block w-100" alt="..."/>
             </div>
             <div class="carousel-item">
-            {/* <img src="..." class="d-block w-100" alt="..."> colocar a imagem*/}
+              <img src="..." class="d-block w-100" alt="..."/>
             </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
-        </button>
+          </button>
         </div>
     </>
   )
